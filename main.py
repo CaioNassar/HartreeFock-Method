f0103c52b8f1d3a7ee2be0fb3e22e80e91ad8c1f
Caio <nassar.s@outlook.com>
2025-09-25 22:13:45 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from atomos import Atom
-from atomos import Geometry
-
-arquivo1 = 'C:\\Users\\caion\\OneDrive\\Documentos\\programation\\python\\ic\\atom.txt'
-arquivo2 = 'C:\\Users\\caion\\OneDrive\\Documentos\\programation\\python\\ic\\mol.txt'
-
-instancia = Atom(arquivo1)
-geometry = Geometry(arquivo2)
-print(instancia.s(0.5,2,1))
