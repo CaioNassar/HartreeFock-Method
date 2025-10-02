9f0b15085b68db18da52bc822802e2082a782cdc
unknown <nassar.s@outlook.com>
2025-10-02 20:46:10 -0300

    - if

--- atoms.py
+++ atoms.py
@@ -45,7 +45,7 @@ class Geometry:
             # Loop through each atom in the element.
             for atom in atom_list:
                 # Create a Basis object for this atom and add it to its dictionary.
-                atom['Data'] = Basis(self.path_basis, self.atomic_number[symbol], atom['Ax'], atom['Ay'], atom['Az'])
+                atom['Data'] = Basis(self.path_basis, symbol, atom['Ax'], atom['Ay'], atom['Az'])
         
 
     def _read(self):
@@ -90,20 +90,36 @@ class Basis:
 
     An instance of this class represents a single atom of the molecule.
     """
+    
+    atomic_number = {
+        'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10,
+        'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20,
+        'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30,
+        'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40,
+        'Nb': 41, 'Mo': 42, 'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50,
+        'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Ce': 58, 'Pr': 59, 'Nd': 60,
+        'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70,
+        'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80,
+        'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90,
+        'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 'Fm': 100,
+        'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109,
+        'Ds': 110, 'Rg': 111, 'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 'Og': 118
+        # This class attribute maps chemical symbols to their respective atomic numbers.
+    }
 
-    def __init__(self, path, a_number, Ax=0, Ay=0, Az=0):
+    def __init__(self, path, symbol, Ax=0, Ay=0, Az=0):
         """
         Initializes the Atom object.
 
         Args:
             path (str): file path to the basis set.
-            a_number (int): atomic number of the element.
+            symbol (str): chemical symbol of the element.
             Ax, Ay, Az (float): coordinates of the atom.
             gaussians (dict): A dictionary to cache basis set data read from the file.
         """
 
         self.path = path
-        self.a_number = a_number
+        self.symbol = symbol
         self.Ax = Ax
         self.Ay = Ay
         self.Az = Az
@@ -129,26 +145,23 @@ class Basis:
             with open(self.path, 'r') as file:
                 lines = list(file)
                 for i, line in enumerate(lines):
-                    if 'inline' in line.lower():
-                        number = float(lines[i+1].strip().split()[0])
-                        if number == self.a_number:
-                            if f'{orbital}-type' in lines[i+2].lower():
-                                # Search the line that specifies the orbital type (e.g. 'S-type', 'p-type', etc.).
-                                num_functions = int(lines[i+3].strip().split()[0])
-                                num_options = int(lines[i+3].strip().split()[1])
+                    if [self.symbol, '/', 'inline'] == line.strip().split():
+                        # Search the flag(/inline) and compare the chemical symbols.
+                        if f'{orbital}-type' in lines[i+2].lower():
+                            # Search the line that specifies the orbital type (e.g. 'S-type', 'p-type', etc.).
+                            num_functions = int(lines[i+3].strip().split()[0])
+                            num_options = int(lines[i+3].strip().split()[1])
 
-                                # Determine line ranges and parse exponents and coefficients.
-                                start_exp = i + 4
-                                end_exp = start_exp + num_functions
-                                for l in lines[start_exp: end_exp]:
-                                    exp.append(l.strip())
+                            # Determine line ranges and parse exponents and coefficients.
+                            start_exp = i + 4
+                            end_exp = start_exp + num_functions
+                            for l in lines[start_exp: end_exp]:
+                                exp.append(l.strip())
 
-                                start_coef = end_exp
-                                end_coef = start_coef + num_functions
-                                for l in lines[start_coef: end_coef]:
-                                    coef.append(l.strip().split())
-
-                                
+                            start_coef = end_exp
+                            end_coef = start_coef + num_functions
+                            for l in lines[start_coef: end_coef]:
+                                coef.append(l.strip().split())
 
             # Dictionary stores the parsed data.
             self.gaussians[orbital] = {
